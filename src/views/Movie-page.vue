74c9ce5ea029e6e322c00149426637c6e5804548
mr.Mhoo <thanathorn@vertice-international.com>
2018-11-20 11:37:02 +0700

    fixsomthing

--- src/views/Movie-page.vue
+++ src/views/Movie-page.vue
@@ -17,67 +17,67 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Axios from "axios";
+import { Component, Vue } from 'vue-property-decorator';
+import Axios from 'axios';
 
 export interface Movie {
-  vote_count: number;
-  id: number;
-  video: boolean;
-  vote_average: number;
-  title: string;
-  popularity: number;
-  poster_path: string;
-  original_language: string;
-  original_title: string;
-  genre_ids: number[];
-  backdrop_path: string;
-  adult: boolean;
-  overview: string;
-  release_date: string;
+    vote_count: number;
+    id: number;
+    video: boolean;
+    vote_average: number;
+    title: string;
+    popularity: number;
+    poster_path: string;
+    original_language: string;
+    original_title: string;
+    genre_ids: number[];
+    backdrop_path: string;
+    adult: boolean;
+    overview: string;
+    release_date: string;
 }
 
 export interface MovieTotal {
-  page?: number;
-  total_results?: number;
-  total_pages?: number;
-  results?: Movie[];
+    page?: number;
+    total_results?: number;
+    total_pages?: number;
+    results?: Movie[];
 }
 
 @Component({})
 export default class MoviePage extends Vue {
-  public MovieData: Movie[] = [];
-  public MovieConfig: MovieTotal = {};
+    public MovieData: Movie[] = [];
+    public MovieConfig: MovieTotal = {};
 
-  public created() {
-    this.getMovie();
-  }
+    public created() {
+        this.getMovie();
+    }
 
-  public getMovie() {
-    // ?language=th-TH
-    const params = {
-      api_key: "22dbd915ba93d8eab2121edf01a8382d"
-    };
+    public getMovie() {
+        // ?language=th-TH
+        const params = {
+            api_key: '22dbd915ba93d8eab2121edf01a8382d',
+        };
 
-    Axios.get("/discover/movie", { params })
-      .then(res => {
-        console.log("getMovie", res);
-        if (res) {
-          const { page, total_results, total_pages, results } = res.data;
+        Axios.get('/discover/movie', { params })
+            .then((res) => {
+                console.log('getMovie', res);
+                if (res) {
+                    const { page, total_results, total_pages, results } = res.data;
 
-          this.MovieData = results;
-          this.MovieData.map(
-            e =>
-              (e.poster_path = `https://image.tmdb.org/t/p/w500${
-                e.poster_path
-              }`)
-          );
-          this.MovieConfig = { page, total_results, total_pages };
-        }
-        console.log("getMovie", this.MovieData, "\n", this.MovieConfig);
-      })
-      .catch(err => console.log("getMovie err", err));
-  }
+                    this.MovieData = results;
+                    this.MovieData.map(
+                        (e) =>
+                            (e.poster_path = `https://image.tmdb.org/t/p/w500${
+                                e.poster_path
+                                }`),
+                    );
+                    this.MovieConfig = { page, total_results, total_pages };
+                }
+                console.log('getMovie', this.MovieData, '\n', this.MovieConfig);
+            })
+            .catch((err) => console.log('getMovie err', err));
+    }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-      ? '/vue-moives/'
-      : '/'
-  }
+    // baseUrl: process.env.NODE_ENV === 'production'
+    //     ? '/vue-moives/'
+    //     : '/',
+    // configureWebpack: {
+    //     output: {
+    //         publicPath: '/vue-moives/'
+    //     }
+    // }
+}
